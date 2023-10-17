a6ee50a1c3c0a080fc3bf55b83591b304288b16b
fishluv <110374653+fishluv@users.noreply.github.com>
2023-10-17 00:43:49 -0700

    update song response

--- app/serializers/song_blueprint.rb
+++ app/serializers/song_blueprint.rb
@@ -1,12 +1,26 @@
 class SongBlueprint < Blueprinter::Base
   identifier :id
-  field :artist
-  field :genre_romantrans
-  field :remywiki_title
-  field :remywiki_url_path
-  field :easy_diff
-  field :normal_diff
-  field :hyper_diff
-  field :ex_diff
-  field :folder
+
+  fields(
+    :title,
+    :genre,
+    :artist,
+    :easy_diff,
+    :normal_diff,
+    :hyper_diff,
+    :ex_diff,
+    :folder,
+    :slug,
+    :remywiki_url_path,
+    :remywiki_title,
+    :genre_romantrans,
+  )
+
+  field :title_sort_char do |song|
+    song.fw_title[0]
+  end
+
+  field :genre_sort_char do |song|
+    song.fw_genre[0]
+  end
 end
