ff83d37cea0739c7f11af387ca109f70d4c5b3dc
fishluv <110374653+fishluv@users.noreply.github.com>
2024-05-07 16:26:30 -0700

    show sql in logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Show SQL queries in logs.
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
