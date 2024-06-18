bad388607c5edcbc199325a70232aa2d6497b44d
fishluv <110374653+fishluv@users.noreply.github.com>
2024-06-18 01:20:23 -0700

    add chart slug diff route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "/charts", to: "search#charts"
   get "/songs", to: "search#songs"
 
+  get "/charts/:slug_diff", to: "fetch#charts"
+
   get "/v2/charts", to: "search#charts", defaults: { version: "v2" }
   get "/v2/charts/:id", to: "fetch#charts"
   get "/v2/songs", to: "search#songs", defaults: { version: "v2" }
