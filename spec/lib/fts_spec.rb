b95ba43afb12b00672d3c299884a1cc0a5e6555b
fishluv <110374653+fishluv@users.noreply.github.com>
2024-08-17 21:25:47 -0700

    add real specs for fts.match_string

--- spec/lib/fts_spec.rb
+++ spec/lib/fts_spec.rb
@@ -1,6 +1,23 @@
-RSpec.describe "Fts" do
+require_relative "../../app/lib/Fts"
+
+RSpec.describe Fts do
   describe ".match_string" do
-    it "passes" do
+    subject { described_class.match_string(query) }
+
+    context "when in general" do
+      let(:query) { "a b c" }
+
+      it "double quotes each term and puts whole thing in single quotes" do
+        is_expected.to eq("'\"a\" \"b\" \"c\"'")
+      end
+    end
+
+    context "when single quotes" do
+      let(:query) { "I'm on fire" }
+
+      it "doubles the single quotes" do
+        is_expected.to eq("'\"I''m\" \"on\" \"fire\"'")
+      end
     end
   end
 end
