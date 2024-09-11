338d18c482688608fe0c47aab74307971e09d14a
fishluv <110374653+fishluv@users.noreply.github.com>
2024-09-10 22:50:27 -0700

    add search endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/charts", to: "search#charts"
   get "/songs", to: "search#songs"
+  get "/search/charts", to: "search#charts"
+  get "/search/songs", to: "search#songs"
 
   get "/charts/:slug/:diff", to: "fetch#charts"
 end
