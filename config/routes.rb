8d04047699983dc3754dbbb9169a823c60346b23
fishluv <110374653+fishluv@users.noreply.github.com>
2024-09-10 21:28:59 -0700

    update old charts endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/charts", to: "search#charts"
   get "/songs", to: "search#songs"
 
-  get "/charts/:slug_diff", to: "fetch#charts"
+  get "/charts/:slug/:diff", to: "fetch#charts"
 
   get "/v2/charts", to: "search#charts"
   get "/v2/songs", to: "search#songs"
