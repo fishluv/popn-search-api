9b6332b436764adfc02a01643d41f3ccf460db33
fishluv <110374653+fishluv@users.noreply.github.com>
2024-09-10 21:34:57 -0700

    delete unused v2 routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,4 @@ Rails.application.routes.draw do
   get "/songs", to: "search#songs"
 
   get "/charts/:slug/:diff", to: "fetch#charts"
-
-  get "/v2/charts", to: "search#charts"
-  get "/v2/songs", to: "search#songs"
-
-  get "/v2/charts/:slug/:diff", to: "fetch#charts"
 end
