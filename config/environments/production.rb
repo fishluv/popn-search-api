999431194692d14d9b96de4d12c5a7c0bd7f31dd
fishluv <110374653+fishluv@users.noreply.github.com>
2024-09-13 13:23:42 -0700

    oops comment out some assets stuff since we got rid of sprockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Commented cuz we don't have assets.
+  # config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
