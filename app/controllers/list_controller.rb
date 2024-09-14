d1a5602eebc9fa711ad9d468b28c79f722709c8c
fishluv <110374653+fishluv@users.noreply.github.com>
2024-09-14 07:33:18 -0700

    add pagy, impl pagination!

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,22 +1,64 @@
 class ListController < ApplicationController
   def charts
     parse_params
-    results = Chart
-    results = results.where(difficulty: @diff) if @diff
-    results = results.where(level: @level) if @level
-    results = results.limit(100)
+    scope = Chart.joins(:song)
+    scope = scope.where("songs.debut = ?", @debut) if @debut.presence
+    scope = scope.where("songs.folder = ?", @folder) if @folder.presence
+    scope = scope.where(difficulty: @diff) if @diff.presence
+    scope = scope.where(level: @level) if @level.presence
 
-    render json: ChartBlueprint.render(results, view: :with_song)
+    @order.each do |order|
+      desc = order.start_with?("-")
+      order.delete_prefix!("-")
+      case order
+      when "title"
+        scope = scope.order("songs.fw_title #{"desc" if desc}")
+      when "genre"
+        scope = scope.order("songs.fw_genre #{"desc" if desc}")
+      when "rtitle"
+        scope = scope.order("songs.remywiki_title #{"desc" if desc}")
+      when "id"
+        scope = scope.order("songs.id #{"desc" if desc}")
+      when "level"
+        scope = scope.order("level #{"desc" if desc}")
+      end
+    end
+
+    @pagy, @records = pagy(scope)
+
+    render json: {
+      data: ChartBlueprint.render_as_hash(@records, view: :with_song),
+      pagy: pagy_metadata(@pagy),
+    }
   end
 
   def songs
     parse_params
-    results = Song
-    results = results.where(debut: @debut) if @debut
-    results = results.where(folder: @folder) if @folder
-    results = results.limit(100)
+    scope = Song
+    scope = scope.where(debut: @debut) if @debut
+    scope = scope.where(folder: @folder) if @folder
 
-    render json: SongBlueprint.render(results, view: :with_charts)
+    @order.each do |order|
+      desc = order.start_with?("-")
+      order.delete_prefix!("-")
+      case order
+      when "title"
+        scope = scope.order("fw_title #{"desc" if desc}")
+      when "genre"
+        scope = scope.order("fw_genre #{"desc" if desc}")
+      when "rtitle"
+        scope = scope.order("remywiki_title #{"desc" if desc}")
+      when "id"
+        scope = scope.order("id #{"desc" if desc}")
+      end
+    end
+
+    @pagy, @records = pagy(scope)
+
+    render json: {
+      data: SongBlueprint.render_as_hash(@records, view: :with_charts),
+      pagy: pagy_metadata(@pagy),
+    }
   end
 
   private
@@ -26,5 +68,6 @@ class ListController < ApplicationController
     @folder = params[:folder]
     @diff = params[:diff]
     @level = params[:level]
+    @order = [params[:order]].flatten.compact
   end
 end
