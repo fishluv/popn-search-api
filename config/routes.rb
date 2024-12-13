3f4c910a2aaf695e4724701cfdb09a396c94ddcf
fishluv <110374653+fishluv@users.noreply.github.com>
2024-12-13 11:12:14 -0800

    switch to new endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get "/charts", to: "search#charts"
-  get "/songs", to: "search#songs"
   get "/search/charts", to: "search#charts"
   get "/search/songs", to: "search#songs"
 
-  get "/charts/:slug/:diff", to: "fetch#charts"
+  get "/fetch/charts/:slug/:diff", to: "fetch#charts"
 
   get "/list/charts", to: "list#charts"
   get "/list/songs", to: "list#songs"
